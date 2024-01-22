573cb4df764502122f32061bfee831c6a76688df
Carlos León <cleon@ucm.es>
2024-01-22 14:47:48 +0100

    inicial 2024

---
+++ sdl.cpp
@@ -0,0 +1,12 @@
+#include <SDL.h>
+
+int main(int argCount, char *args[]) {
+  SDL_Window *window =
+      SDL_CreateWindow("SDL!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       800, 400, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+
+  SDL_Delay(4000);
+
+  SDL_DestroyWindow(window);
+  return 0;
+}
